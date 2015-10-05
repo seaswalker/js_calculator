a511a0981b9f332af905e9d2a6a008815b775334
seaswalker <xsdwem7@hotmail.com>
2015-10-05 19:14:46 +0800

    重写js

--- js/cal.js
+++ js/cal.js
@@ -1,203 +1,213 @@
-var cal = {
-	"mouseHoverColor": "#CFCFCF",
-	"mouseOutColor": "#E6E6E6",
-	"firstMouseOutColor": "#F2F2F2",
-	"showInput": null,
-	"preStep": null,
-	//二元运算符
-	"binaryOperator": null
-};
-
 window.onload = function() {
-	init();
-	keyHover();
-	clearAll();
-	back();
-	ce();
-};
-
-function init() {
-	cal.showInput = document.getElementById("show-input");
-	cal.preStep = document.getElementById("pre-step");
+    Calculator.initCache();
+    Calculator.initListeners();
 }
 
-/**
- * [clearAll 重置所有]
- */
-function clearAll() {
-	document.getElementById("clear-all").onclick = function() {
-		cal.showInput.innerHTML = "0";
-		cal.preStep.innerHTML = "&nbsp;";
-		cal.binaryOperator = null;
-	}
-}
-
-/**
- * [back 设置退格键]
- */
-function back() {
-	document.getElementById("back").onclick = function() {
-		var oldValue = cal.showInput.innerText;
-		if (oldValue.length < 2) {
-			var newValue = "";
-			newValue = "0";
-		} else {
-			newValue = oldValue.substring(0, oldValue.length - 1);
-		}
-		cal.showInput.innerHTML = newValue;
-	}
-}
-
-/**
- * [ce 清空showInput]
- */
-function ce() {
-	document.getElementById("ce").onclick = function() {
-		cal.showInput.innerHTML = "0";
-	}
-}
-
-/**
- * [handleKey 处理按键按下事件]
- * @param value 键值
- */
-function handleKey(value) {
-	if (!isNaN(value) || value === ".") {
-		showInput(value);
-	} else {
-		switch (value) {
-			case "±":
-				unaryOperate(value, function(v) {
-					var oldValue = cal.showInput.innerText;
-					if (oldValue === "0") {
-						return oldValue;
-					}
-					if (oldValue.charAt(0) === '-') {
-						return oldValue.substring(1);
-					} else {
-						return "-" + oldValue;
-					}
-				});
-				break;
-			case "√":
-				unaryOperate(value, function(v) {
-					var result = Math.sqrt(cal.showInput.innerText);
-					return checkLength(result);
-				});
-				break;
-			case "x²":
-				unaryOperate(value, function(v) {
-					var result = Math.pow(cal.showInput.innerText, 2);
-					return checkLength(result);
-				});
-				break;
-			case "½":
-				unaryOperate(value, function(v) {
-					var oldValue = cal.showInput.innerText;
-					if (oldValue === "0") {
-						return "0不能作为除数";
-					}
-					var result = 1 / oldValue;
-					return checkLength(result);
-				});	
-				break;
-			case "+":
-			case "-":
-			case "%":
-				binaryOperate(value);
-				break;
-			case "×":
-				binaryOperate("*");
-				break;
-			case "÷":
-				binaryOperate("/");
-				break;
-			case "=":
-				calculate();
-				break;
-		}
-	}
-}
-
-/**
- * [checkLength 确保结果长度不大于13]
- */
-function checkLength(value) {
-	var valueStr = value + "";
-	if (valueStr.length > 12) {
-		//转化为2位小数的指数计数法
-		value = value.toExponential(2);
-	}
-	return value;
-}
-
-/**
- * [unaryOperate 执行一元运算]
- * 比如取反、取余
- */
-function unaryOperate(value, operation) {
-	cal.showInput.innerHTML = operation(value);
-}
-
-/**
- * [binaryOperate 二元操作]
- * 存在两种情况:
- * a) 如果之前输入过操作符，那么首先计算第一步操作
- * b) 如果之前没有，那么需要把第一个操作数和运算符显示到pre-step
- */
-function binaryOperate(operator, operation) {
-	if (cal.binaryOperator === null) {
-		cal.preStep.innerHTML = cal.showInput.innerText + " " + operator;
-	} else {
-		//计算上一步
-		cal.preStep.innerHTML = eval(cal.preStep.innerText + cal.showInput.innerText) + " " + operator;
-	}
-	cal.binaryOperator = operator;
-	cal.showInput.innerHTML = "0";
-}
-
-/**
- * [calculate 按下=时计算最终结果]
- */
-function calculate() {
-	cal.showInput.innerHTML = checkLength(eval(cal.preStep.innerText + cal.showInput.innerText));
-	cal.preStep.innerHTML = "&nbsp;";
-	cal.binaryOperator = null;
-}
-
-/**
- * [showInput 显示输入内容]
- */
-function showInput(value) {
-	var oldValue = cal.showInput.innerText;
-	var newValue = oldValue;
-	if (oldValue === "0" && value !== ".") {
-		newValue = value;
-	}
-	//只支持13位数
-	else if (oldValue.length < 13) {
-		newValue += value;	
-	}
-	cal.showInput.innerHTML = newValue;
-}
-
-//按键鼠标悬浮效果
-function keyHover() {
-	var lis = document.getElementsByTagName("li");
-	var key = null;
-	for (var i = 0;i < lis.length;i ++) {
-		key = lis[i];
-		key.onmouseover = function() {
-			this.style.backgroundColor = cal.mouseHoverColor;
-		};
-		key.onmouseout = _helper(i, key);
-		key.onclick = function() {
-			handleKey(this.innerText);
-		};
-	}
-	function _helper(i, li) {
-		return function() {
-			li.style.backgroundColor =  i < 4 ? cal.firstMouseOutColor : cal.mouseOutColor;
-		};
-	}
-}
+//全局计算器对象
+var Calculator = (function() {
+	var cal = {
+		//常量
+		constants: {
+			//鼠标悬停时的颜色
+			mouseHoverColor: "#CFCFCF",
+			//计算器第一行和下面其它行的颜色是不同的，这个是第一行的背景颜色
+			firstMouseOutColor: "#F2F2F2",
+			//剩余各行的背景颜色
+			mouseOutColor: "#E6E6E6"
+		},
+		cache: {
+			//输入内容显示元素
+			showInput: null,
+			//上一步计算结果显示区域
+			preStep: null,
+			//二元运算符
+			binaryOperator: null
+		},
+        /**
+         * 初始化缓存对象(cal.cache)
+         */
+        initCache: function() {
+            cal.cache.showInput = document.getElementById("show-input");
+            cal.cache.preStep = document.getElementById("pre-step");
+        },
+		//初始化事件监听器
+		initListeners: function() {
+			//清空键(即C)
+			cal.addEvent(document.getElementById("clear-all"), "click", function() {
+				cal.cache.showInput.innerHTML = "0";
+				cal.cache.preStep.innerHTML = "&nbsp;";
+				cal.cache.binaryOperator = null;
+			});
+			//退格键("Back")
+			cal.addEvent(document.getElementById("back"), "click", function() {
+				var oldValue = cal.cache.showInput.innerText;
+				cal.cache.showInput.innerHTML = oldValue.length < 2 ? "0" : oldValue.substring(0, oldValue.length - 1);
+			});
+			//CE键(只清空show-input元素)
+            cal.addEvent(document.getElementById("ce"), "click", function() {
+                cal.cache.showInput.innerHTML = "0";
+            });
+            var lis = document.getElementsByTagName("li"), key;
+            for (var i = 0;i < lis.length;i ++) {
+                key = lis[i];
+                //按键鼠标悬浮变色效果
+                cal.addEvent(key, "mouseover", function() {
+                    this.style.backgroundColor = cal.constants.mouseHoverColor;
+                });
+                //鼠标移出后颜色变回正常
+                cal.addEvent(key, "mouseout", _helper(i, key));
+                //按键点击事件
+                cal.addEvent(key, "click", function() {
+                    cal.handleKey(this.innerText);
+                });
+            }
+            function _helper(i, li) {
+                return function() {
+                    li.style.backgroundColor =  i < 4 ? cal.constants.firstMouseOutColor : cal.constants.mouseOutColor;
+                };
+            }
+		},
+        /**
+         * 相应按键按下事件
+         * @param value 按键的内容(innerText)
+         */
+        handleKey: function(value) {
+            //如果是一个数字或者小数点，直接显示出来
+            if (!isNaN(value) || value === ".") {
+                cal.showInput(value);
+            } else {
+                switch (value) {
+                    case "±":
+                        cal.unaryOperate(function() {
+                            var oldValue = cal.cache.showInput.innerText;
+                            if (oldValue === "0") {
+                                return oldValue;
+                            }
+                            if (oldValue.charAt(0) === '-') {
+                                return oldValue.substring(1);
+                            } else {
+                                return "-" + oldValue;
+                            }
+                        });
+                        break;
+                    case "√":
+                        cal.unaryOperate(function() {
+                            var result = Math.sqrt(cal.cache.showInput.innerText);
+                            return cal.checkLength(result);
+                        });
+                        break;
+                    case "x²":
+                        cal.unaryOperate(function() {
+                            var result = Math.pow(cal.cache.showInput.innerText, 2);
+                            return cal.checkLength(result);
+                        });
+                        break;
+                    case "½":
+                        cal.unaryOperate(function() {
+                            var oldValue = cal.cache.showInput.innerText;
+                            if (oldValue === "0") {
+                                return "0不能作为除数";
+                            }
+                            var result = 1 / oldValue;
+                            return cal.checkLength(result);
+                        });
+                        break;
+                    case "+":
+                    case "-":
+                    case "%":
+                       cal. binaryOperate(value);
+                        break;
+                    case "×":
+                        //乘法和触发不可以直接传递value，因为计算机不认识X和÷
+                        cal.binaryOperate("*");
+                        break;
+                    case "÷":
+                        cal.binaryOperate("/");
+                        break;
+                    case "=":
+                        cal.calculate();
+                        break;
+                }
+            }
+        },
+        /**
+         * 执行一元运算 比如取反、取余
+         * @param operation 具体运算回调函数
+         */
+        unaryOperate: function(operation) {
+            cal.cache.showInput.innerHTML = operation();
+        },
+        /**
+         * 二元操作(+ - * / %)
+         * 存在两种情况:
+         * a) 如果之前输入过操作符，那么首先计算第一步操作
+         * b) 如果之前没有，那么需要把第一个操作数和运算符显示到pre-step
+         * @param operator 操作符
+         */
+        binaryOperate: function(operator) {
+            if (cal.cache.binaryOperator == null) {
+                cal.cache.preStep.innerHTML = cal.cache.showInput.innerText + " " + operator;
+            } else {
+                //计算上一步
+                cal.cache.preStep.innerHTML = eval(cal.cache.preStep.innerText + cal.cache.showInput.innerText) + " " + operator;
+            }
+            cal.cache.binaryOperator = operator;
+            cal.cache.showInput.innerHTML = "0";
+        },
+        /**
+         * [calculate 按下=时计算最终结果]
+         */
+        calculate: function() {
+            cal.cache.showInput.innerHTML = cal.checkLength(eval(cal.cache.preStep.innerText + cal.cache.showInput.innerText));
+            cal.cache.preStep.innerHTML = "&nbsp;";
+            cal.cache.binaryOperator = null;
+        },
+        /**
+         * 确保结果长度不大于13
+         * @param value 需要检查的结果
+         */
+        checkLength: function(value) {
+            var valueStr = value + "";
+            if (valueStr.length > 12) {
+                //转化为2位小数的指数计数法
+                value = value.toExponential(2);
+            }
+            return value;
+        },
+        /**
+         * 显示输入的内容
+         * 用于相应数字/小数点按键
+         * @param value 按键的内容
+         */
+        showInput: function(value) {
+            var oldValue = cal.cache.showInput.innerText;
+            var newValue = oldValue;
+            if (oldValue === "0" && value !== ".") {
+                newValue = value;
+            }
+            //只支持13位数
+            else if (oldValue.length < 13) {
+                newValue += value;
+            }
+            cal.cache.showInput.innerHTML = newValue;
+        },
+        /**
+         * 工具方法，为element添加事件处理函数
+         * @param element 需要添加事件的dom元素
+         * @param name name事件名称(不含on)
+         * @param handler 事件处理函数
+         */
+        addEvent: function(element, name, handler) {
+            if (window.addEventListener) {
+                element.addEventListener(name, function(event) {
+                    handler.call(element, event);
+                }, true);
+            } else if (window.attachEvent) {
+                element.attachEvent("on" + name, function(event) {
+                    handler.call(element, event);
+                });
+            }
+        }
+	};
+	return cal;
+})();
