30987d0ced01e31858ff495ac80b417d7cd4a7a7
seaswalker <xsdwem7@hotmail.com>
2015-10-07 12:49:58 +0800

    完成科学型

--- js/cal.js
+++ js/cal.js
@@ -33,7 +33,7 @@ var Calculator = (function() {
           22: ')',
           23: 'yroot',
           24: 'n!',
-          25: 'Mod',
+          25: 'Exp',
           26: '^',
           27: 'sin',
           28: 'cos',
@@ -44,7 +44,20 @@ var Calculator = (function() {
           33: 'cosh',
           34: 'tanh',
           35: 'π',
-          36: '↑'
+          36: '↑',
+          37: 'CE',
+          38: 'C',
+          39: 'Back'
+        },
+        //映射用于显示的操作符，比如计算时用*，而显示时x更好
+        operatorFacade: {
+            13: '+',
+            14: '-',
+            15: '×',
+            16: '÷',
+            17: '%',
+            23: 'yroot',
+            26: '^'
         },
         //当前计算器的类型1 --> 标准型, 2-->科学型，默认标准型
         type: 1,
@@ -73,27 +86,37 @@ var Calculator = (function() {
 			//上一步计算结果显示区域
 			preStep: null
 		},
-        //记录上次运算的结果
-        preResult: 0,
-        //记录上次的运算符
-        preOperator: null,
-        //记录上次的运算函数
-        preCallback: null,
+        //操作数栈
+        operandStack: [],
+        //运算符栈
+        operatorStack: [],
         //上一次输入是否是二元运算符，如果是并且再次输入二元运算符，那么忽略此次输入
         isPreInputBinaryOperator: false,
+        //上次按键是否是一元操作
+        isPreInputUnaryOperator: false,
+        //等号不可以连按
+        isPreInputEquals: false,
         //如果为true，那么接下来输入的数字需要覆盖在showInput上，而不是追加
+        //上一次计算的结果(=)
+        preResult: 0,
         isOverride: false,
         //int校验
         intPattern: /^-?\d+$/,
         //小数校验
         floatPattern: /^-?\d+\.\d+$/,
+        //科学计数法校验
+        scientificPattern: /^\d+\.\d+e(\+|-)\d+$/,
         //辅助判断运算符的优先级
         operatorPriority: {
+            ")": 0,
             "+": 1,
             "-": 1,
             "*": 2,
             "%": 2,
-            "/": 2
+            "/": 2,
+            "^": 3,
+            "yroot": 3,
+            "(": 4
         },
         /**
          * 初始化缓存对象(cal.cache)
@@ -106,21 +129,6 @@ var Calculator = (function() {
 		//初始化事件监听器
 		initListeners: function() {
             var prefix = cal.typePrefix[cal.type];
-			//清空键(即C)
-			cal.addEvent(document.getElementById(prefix + "clear-all"), "click", function() {
-				cal.cache.showInput.innerHTML = "0";
-				cal.cache.preStep.innerHTML = "&nbsp;";
-                cal.preResult = 0;
-			});
-			//退格键("Back")
-			cal.addEvent(document.getElementById(prefix + "back"), "click", function() {
-				var oldValue = cal.cache.showInput.innerText;
-				cal.cache.showInput.innerHTML = oldValue.length < 2 ? "0" : oldValue.substring(0, oldValue.length - 1);
-			});
-			//CE键(只清空show-input元素)
-            cal.addEvent(document.getElementById(prefix + "ce"), "click", function() {
-                cal.cache.showInput.innerHTML = "0";
-            });
             //设置运算符事件监听
             var lis = document.getElementById(prefix + "top-symbol").getElementsByTagName("li");
             _helper(lis, cal.constants.firstMouseOutColor);
@@ -230,6 +238,12 @@ var Calculator = (function() {
                             return [cal.fact(si), "fact"];
                         });
                         break;
+                    //Exp 转为科学计数法表示
+                    case 25:
+                        cal.unaryOperate(function(si) {
+                            return [si.toExponential(7)];
+                        });
+                        break;
                     //sin
                     case 27:
                         cal.unaryOperate(function(si) {
@@ -292,24 +306,50 @@ var Calculator = (function() {
                     case 15:
                     case 16:
                     case 17:
+                    //x的y次方
+                    case 26:
+                    //开任意次方根
+                    case 23:
                         if (cal.isPreInputBinaryOperator) {
                             break;
                         }
                         cal.isPreInputBinaryOperator = true;
                         cal.isOverride = true;
-                        var operator = cal.keyCodes[keyCode];
-                        cal.binaryOperate(operator, function(pr, po, si) {
-                            return po == null ? si : eval(pr + po + si);
-                        });
+                        cal.binaryOperate(cal.keyCodes[keyCode], cal.operatorFacade[keyCode]);
                         break;
                     case 12:
                         cal.calculate();
+                        break;
+                    //ce
+                    case 37:
+                        cal.ce();
+                        break;
+                    //c
+                    case 38:
+                        cal.clear();
+                        break;
+                    //back
+                    case 39:
+                        cal.back();
+                        break;
+                    // (
+                    case 21:
+                        cal.cache.preStep.innerHTML += " (";
+                        cal.operatorStack.push("(");
+                        break;
+                    // )
+                    case 22:
+                        cal.rightTag();
+                        break;
+                    //向上箭头，把上次计算结果显示出来
+                    case 36:
+                        cal.cache.showInput.innerHTML = cal.preResult;
                         break;
                 }
             }
         },
         /**
-         * 执行一元运算 比如取反、取余
+         * 执行一元运算 比如取倒数、平方
          * @param operation 具体运算回调函数
          * 会向operation传递一个参数si，为用户当前的输入，同时operation函数应该返回一个数组，数组的第一个
          * 元素是计算的结果，第二个元素示例sqrt，第二个参数可选
@@ -318,13 +358,25 @@ var Calculator = (function() {
             var si = cal.cache.showInput.innerHTML, result;
             if (cal.isInteger(si)) {
                 result = operation(parseInt(si));
-            } else if (cal.isFloat(si)) {
+            } else if (cal.isFloat(si) || cal.isScientific(si)) {
                 result = operation(parseFloat(si));
             }
             if (result != null) {
                 cal.cache.showInput.innerHTML = cal.checkLength(result[0]);
                 if (result.length > 1) {
-                    cal.cache.preStep.innerHTML += (" " + result[1] + "(" + si + ")");
+                    //显示prestep有两种情况:
+                    //第一种就是这是第一次(指连续调用的第一次)调用一元函数，此时直接接在末尾即可
+                    if (!cal.isPreInputUnaryOperator) {
+                        cal.cache.preStep.innerHTML += (" " + result[1] + "(" + si + ")");
+                        cal.isPreInputUnaryOperator = true;
+                    } else {
+                        //第二种就是这不是第一次，那么应该截取最后一个空格之后的内容进行替换
+                        //比如1 + 3 + sqrt(100)，那么应该从最后一个空格后替换为此次操作的内容
+                        var pi = cal.cache.preStep.innerHTML;
+                        pi = pi.substring(0, pi.lastIndexOf(" "));
+                        pi += (" " + result[1] + "(" + si + ")");
+                        cal.cache.preStep.innerHTML = pi;
+                    }
                 }
                 //一元运算结束后应该覆盖
                 cal.isOverride = true;
@@ -334,28 +386,122 @@ var Calculator = (function() {
         /**
          * 二元操作(+ - * / %)
          * @param operator 操作符
-         * @param callback 回调函数，根据上一步的结果和此时的输入计算出新的结果
-         * 接受三个参数:上两步的结果，上一步的运算符和当前输入
+         * @param facade 运算符门面，用于显示在preStep中
          */
-        binaryOperate: function(operator, callback) {
-            var si = cal.cache.showInput.innerHTML;
+        binaryOperate: function(operator, facade) {
+            var si = cal.cache.showInput.innerHTML,
+                pi = cal.cache.preStep.innerHTML;
             if (cal.isNumber(si)) {
-               cal.cache.preStep.innerHTML += (" " + si + " " + operator);
-               //显示这一步的运算结果
-               cal.cache.showInput.innerHTML = cal.preResult = callback(cal.preResult, cal.preOperator, si);
-               cal.preOperator = operator;
-               cal.preCallback = callback;
+                //压操作数栈
+                cal.operandStack.push(si);
+                //设置preStep有三种情况:第一种上一步不是一元操作，那么需要设置si，第二种是一元操作，那么由于一元操作会把
+                //函数表达式(比如sqrt(100))设置到preStep，所以不需要再次设置si
+                //第三种就是如果最后一位是右括号，那么也不需要设置si
+                cal.cache.preStep.innerHTML += (cal.isPreInputUnaryOperator || pi.charAt(pi.length - 1) === ")")
+                    ? (" " + facade) : (" " + si + " " + facade);
+                var preOp = cal.operatorStack.pop();
+                if (preOp != null) {
+                    var op = cal.operatorPriority[operator],
+                        pp = cal.operatorPriority[preOp];
+                    //如果当前运算符优先级更高，那么只需压栈不需要计算
+                    if (op > pp) {
+                        cal.operatorStack.push(preOp);
+                    }
+                    //两者的优先级相等并且高于1，那么只需要计算一步
+                    else if (op > 1 && op === pp) {
+                        cal.operatorStack.push(preOp);
+                        cal._travelStack(1);
+                    }
+                    else {
+                        cal.operatorStack.push(preOp);
+                        cal.cache.showInput.innerHTML = cal.checkLength(cal._travelStack(null, op));
+                    }
+                }
+                cal.operatorStack.push(operator);
             }
+            cal.isPreInputUnaryOperator = false;
+            cal.isPreInputEquals = false;
         },
         /**
          * 按下=时计算最终结果
          */
         calculate: function() {
+            if (!cal.isPreInputEquals) {
+                var si = cal.cache.showInput.innerHTML, result;
+                if (cal.isNumber(si)) {
+                    cal.operandStack.push(si);
+                    result = cal.checkLength(cal._travelStack());
+                    cal.cache.showInput.innerHTML = result;
+                    cal.preResult = result;
+                    cal.cache.preStep.innerHTML = "&nbsp;";
+                    cal.isOverride = true;
+                }
+                cal._reset();
+                cal.isPreInputEquals = true;
+            }
+        },
+        /**
+         * 访问运算栈，返回计算结果
+         * @param level 计算的层数，如果不指定，那么遍历整个栈
+         * @param minPri(最小/截止优先级) 此参数针对下面的情况:
+         * 2 + 2 X 3 X 2 ^ 2 X 2，由于最后一个运算符是X，优先级比^低，所以触发了对操作栈的遍历，但是不能全部遍历，应该遍历到第一个X停止
+         * 如果不停止得到的将是错误的26 X 2 = 52，正确结果是2 + 24 X 2 = 50
+         * @return Number
+         * @private
+         */
+        _travelStack: function(level, minPri) {
+            var op, f, s,
+                //result取操作数栈栈顶，因为防止在下列情况9 X (6 + 时出现undefined
+                result = cal.operandStack[cal.operandStack.length - 1],
+                l = level || cal.operatorStack.length,
+                p = minPri || 0;
+            for (var i = 0;i < l;++ i) {
+                op = cal.operatorStack.pop();
+                //遇到minPri或左括号立即停止，左括号也需要再次压入，因为只有一个右括号才能抵消一个左括号
+                if (cal.operatorPriority[op] < p || op === "(") {
+                    cal.operatorStack.push(op);
+                    break;
+                }
+                s = cal.operandStack.pop();
+                f = cal.operandStack.pop();
+                if (op === "^") {
+                    result = Math.pow(f, s);
+                } else if (op === "yroot") {
+                    result = Math.pow(f, 1 / s);
+                } else {
+                    result = eval(f + op + s);
+                }
+                cal.operandStack.push(result);
+            }
+            return result;
+        },
+        /**
+         * 输入了一个右括号
+         */
+        rightTag: function() {
             var si = cal.cache.showInput.innerHTML;
             if (cal.isNumber(si)) {
-                cal.cache.showInput.innerHTML = cal.checkLength(cal.preCallback(cal.preResult, cal.preOperator, si));
-                cal.cache.preStep.innerHTML = "&nbsp;";
-                cal.isOverride = true;
+                cal.cache.preStep.innerHTML += (" " + si + " )");
+                cal.operandStack.push(si);
+                //遍历计算操作栈，直至遇到左括号
+                var op = cal.operatorStack.pop(),
+                    f, s, result;
+                while (op !== "(" && op != null) {
+                    s = cal.operandStack.pop();
+                    f = cal.operandStack.pop();
+                    if (op === "^") {
+                        result = Math.pow(f, s);
+                    } else if (op === "yroot") {
+                        result = Math.pow(f, 1 / s);
+                    } else {
+                        result = eval(f + op + s);
+                    }
+                    cal.operandStack.push(result);
+                    op = cal.operatorStack.pop();
+                }
+                //此处应该直接把小括号的计算内容弹出，因为此结果显示在了showInput中，而再次执行二元操作时会先有一个压栈的操作，
+                // 并且执行=时也是根据showInput内容计算的
+                cal.cache.showInput.innerHTML = cal.checkLength(cal.operandStack.pop());
             }
         },
         /**
@@ -369,13 +515,27 @@ var Calculator = (function() {
             }
             return valueStr.length > 12 ? value.toExponential(7) : valueStr;
         },
+        //CE
+        ce: function() {
+            cal.cache.showInput.innerHTML = "0";
+        },
+        //C
+        clear: function() {
+            cal.cache.showInput.innerHTML = "0";
+            cal.cache.preStep.innerHTML = "&nbsp;";
+            cal._reset();
+        },
+        back: function() {
+            var oldValue = cal.cache.showInput.innerText;
+            cal.cache.showInput.innerHTML = oldValue.length < 2 ? "0" : oldValue.substring(0, oldValue.length - 1);
+        },
         /**
          * 校验字符串是否是数字
          * @param str
          * @return 是返回true
          */
         isNumber: function(str) {
-            return cal.isInteger(str) || cal.isFloat(str);
+            return cal.isInteger(str) || cal.isFloat(str) || cal.isScientific(str);
         },
         /**
          * 校验是否是整数
@@ -390,6 +550,13 @@ var Calculator = (function() {
          */
         isFloat: function(str) {
             return str.match(cal.floatPattern);
+        },
+        /**
+         * 是否是科学计数法
+         * @param str
+         */
+        isScientific: function(str) {
+            return str.match(cal.scientificPattern);
         },
         /**
          * 显示输入的内容
@@ -420,6 +587,8 @@ var Calculator = (function() {
             cal.cache.showInput.innerHTML = newValue;
             cal.isOverride = false;
             cal.isPreInputBinaryOperator = false;
+            cal.isPreInputUnaryOperator = false;
+            cal.isPreInputEquals = false;
         },
         /**
          * 切换计算器类型
@@ -434,9 +603,18 @@ var Calculator = (function() {
                 cal.hasInited[type] = true;
             }
             cal.initCache();
-            cal.preOperator = null;
-            cal.preResult = 0;
+            cal._reset();
+        },
+        /**
+         * 重置各个标志变量以及操作栈
+         * @private
+         */
+        _reset: function() {
+            cal.operandStack = [];
+            cal.operatorStack = [];
             cal.isPreInputBinaryOperator = false;
+            cal.isPreInputUnaryOperator = false;
+            cal.isPreInputEquals = false;
         },
         /**
          * 工具方法，为element添加事件处理函数
